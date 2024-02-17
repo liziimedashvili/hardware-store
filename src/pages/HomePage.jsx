852e6953a7d7e68a4c40c69559bac42365e125c3
unknown <you@example.com>
2024-02-17 21:10:24 +0400

    carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,15 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import Categories from '../components/categories/Categories'
+import React from 'react';
+import Categories from '../components/categories/Categories';
+import SimpleSlider from '../components/slider/Slider';
+
 export default function HomePage() {
   return (
-    <div>
-      <Categories/>
+    <div className='custom-container'>
+      <div className='flex flex-row justify-between'>
+        <Categories />
+        <SimpleSlider />
+      </div>
     </div>
-  )
+  );
 }
